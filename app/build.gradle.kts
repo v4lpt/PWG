76ae58cf3ce42a71e94acba2f7391fa40dd1fa91
v4lpt <v4lpt@posteo.net>
2024-01-04 15:25:45 +0100

    refractored package to v4lpt.vpt.pwg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("tools.fastlane:screengrab:2.1.1")
     implementation ("com.google.android.material:material:1.4.0")
 
 
--- app/src/androidTest/java/v4lpt/vpt/pwg/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/v4lpt/vpt/pwg/ExampleInstrumentedTest.java
@@ -1,12 +1,27 @@
 package v4lpt.vpt.pwg;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+import tools.fastlane.screengrab.cleanstatusbar.BluetoothState;
+import tools.fastlane.screengrab.cleanstatusbar.CleanStatusBar;
+import tools.fastlane.screengrab.cleanstatusbar.MobileDataType;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+import tools.fastlane.screengrab.*;
+
 
 import static org.junit.Assert.*;
 
@@ -17,10 +32,27 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    //@Rule
+    //public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testTakeScreenshot() {
+        Screengrab.screenshot("before_button_click");
+
+        // Your custom onView...
+        //onView(withId(R.id.generateButton)).perform(click());
+
+        Screengrab.screenshot("after_button_click");
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.appgen2", appContext.getPackageName());
+        assertEquals("v4lpt.vpt.pwg", appContext.getPackageName());
     }
 }
